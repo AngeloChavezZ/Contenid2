17fc3e7d9275bf90fd402b1551278b4b912a450e
Angelo Chávez Z <angelozamora88@gmail.com>
2024-11-03 19:19:09 -0500

    Se ha realizado el cambio en poo y a su vez la prueba del funcionamiento del programa

--- src/poo/PruebaAudioVisual.java
+++ src/poo/PruebaAudioVisual.java
@@ -2,18 +2,33 @@ package poo;
 import uni1a.*;
 
 public class PruebaAudioVisual {
-	public static void main(String[] args) {
-        System.out.println("Hello from Eclipse!");
+    public static void main(String[] args) {
+        // Crear y probar una película
+        Pelicula pelicula = new Pelicula("Inception", 148, "Ciencia Ficción", "Warner Bros");
+        Actor actor = new Actor("Leonardo DiCaprio", 45, "Protagonista");
+        pelicula.agregarActor(actor);
+        pelicula.mostrarDetalles();
 
-        // Crear instancias de las subclases
-        ContenidoAudiovisual[] contenidos = new ContenidoAudiovisual[3];
-        contenidos[0] = new Pelicula("Avatar", 125, "Accion", "20th Century Studios");
-        contenidos[1] = new SerieDeTV("Game of Thrones", 60, "Fantasy", 8);
-        contenidos[2] = new Documental("Cosmos", 45, "Science", "Astronomy");
+        // Crear y probar una serie de TV
+        SerieDeTV serie = new SerieDeTV("Stranger Things", 50, "Ciencia Ficción");
+        Temporada temporada1 = new Temporada(1, java.util.Arrays.asList("Episodio 1", "Episodio 2"));
+        serie.agregarTemporada(temporada1);
+        serie.mostrarDetalles();
 
-        // Mostrar los detalles de cada contenido audiovisual
-        for (ContenidoAudiovisual contenido : contenidos) {
-            contenido.mostrarDetalles();
-        }
+        // Crear y probar un documental
+        Documental documental = new Documental("Nuestro Planeta", 90, "Naturaleza", "Medio Ambiente");
+        Investigador investigador = new Investigador("Dr. Jane Goodall", "Biología", "Universidad de Cambridge");
+        documental.agregarInvestigador(investigador);
+        documental.mostrarDetalles();
+
+        // Crear y probar un video de YouTube
+        VideoYouTube video = new VideoYouTube("Java Tutorial", 15, "Educativo", "ProgrammingWithMosh", 100000);
+        video.mostrarDetalles();
+
+        // Crear y probar un cortometraje
+        Cortometraje corto = new Cortometraje("La Partida", 20, "Drama", "Carlos Lopez", 20);
+        corto.mostrarDetalles();
     }
 }
+
+
